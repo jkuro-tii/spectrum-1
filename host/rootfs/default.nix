326c148a479228ce393a745fec3a17aa18bbd720
jkuro-tii <jaroslaw.kurowski@tii.ae>
2022-09-06 14:58:19 +0400

    qemu added to the build.

--- host/rootfs/default.nix
+++ host/rootfs/default.nix
@@ -20,8 +20,10 @@ let
 
   foot = pkgsGui.foot.override { allowPgo = false; };
 
+  qemu = pkgs.pkgsMusl.qemu.override { gtkSupport = false; sdlSupport = false; };
+
   packages = [
-    cloud-hypervisor execline jq kmod mdevd s6 s6-linux-init s6-rc socat
+    qemu cloud-hypervisor execline jq kmod mdevd s6 s6-linux-init s6-rc socat
     start-vm
 
     (cryptsetup.override {
